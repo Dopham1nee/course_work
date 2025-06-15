994057747a7d9bf0fc0789e3b73435cf3edc0084
Sigma1337 <Sigma1337@Sigmaputer.localdomain>
2025-06-15 17:12:52 +0300

    v1.1.0
    final version with all required features

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import json
 import logging as log
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+import sympy_solve as sp_solve
+
 # read json input and parse it
-def read_input(filename: str) -> tuple[float, float, float, list[float]]:
+def read_input(filename: str) -> tuple[bool, float, float, float, list[float]]:
     with open(filename) as f:
         input = json.load(f)
-        return input['r'], input['v0'], input['s_max'], input['h']
+        return input['debug'], input['r'], input['v0'], input['s_max'], input['h']
 
 
-def f(v0, s, r) -> float: return v0 * np.exp(s ** 2 / 2)
+def f(v0, s, r) -> float: return v0 * np.exp(-s / r)
 
 
-def rhs(v, s) -> float: return v * s
+def rhs(v, r) -> float: return -v / r
 
 
 def richardson_accuracy(y_h, y_h2, y_h4):
@@ -30,24 +33,78 @@ def richardson_accuracy(y_h, y_h2, y_h4):
 
 
 
-def calc_err(x_analytics, x, method: str, h: float, logger) -> float:
-    logger.debug('Starting calculate %s errors for h = %.4f', method, h)
+def calc_err(y_analytics, y, method: str, h: float, logger) -> float:
+    if method != 'none':
+        logger.debug('Starting calculate %s errors for h = %.3f', method, h)
 
     errs = []
-    for i in range(0, len(x_analytics)):
-        err = np.abs((x[i] - x_analytics[i]) / x_analytics[i]) * 100
+    for i in range(0, len(y_analytics)):
+        err = np.abs((y[i] - y_analytics[i]) / y_analytics[i]) * 100
+        if err == 100:
+            print(f"{y_analytics[i]}, {y[i]}")
         errs.append(err)
-        logger.debug('%s_err[%i] = %.8f', method, i, err)
+        if method != 'none': logger.debug('%s_err[%i] = %.8f', method, i, err)
 
     max_err = np.max(np.array(errs))
-    logger.warning('Error for h = %.4f and %s method: %.8f', h, method, max_err)
+    if method != 'none':
+        logger.warning('Error for h = %.3f and %s method: %.8f', h, method, max_err)
 
     return max_err
+
+
+def euler_round(x: np.ndarray, h, n, start, b):
+    x[0] = start
+    for i in range(n):
+        x[i + 1] = x[i] + h * rhs(x[i], b)
+
+    return x
+
+
+def adams_round(y: np.ndarray, h, n, start, b):
+    # first 4 points from euler
+    y[:4] = euler_round(y[:4], h, 3, start, b)
+    
+    for i in range(3, n):
+        y[i + 1] = y[i] + (h / 24) * (
+            55 * rhs(y[i], b) -
+            59 * rhs(y[i - 1], b) +
+            37 * rhs(y[i - 2], b) -
+            9 * rhs(y[i - 3], b)
+        )
+    
+    return y
+
+
+def find_opt_grid(
+    h: float,
+    err: float,
+    start: float,
+    y_max: float,
+    b: float,
+    method,
+):
+    while 0.01 > err or err > 0.1:
+        if err > 0.1:
+            h /= 2
+        elif err < 0.01:
+            h *= 2
+
+        x_vals = np.arange(0, y_max + h, h)
+        y_analytics = np.array([f(start, x, b) for x in x_vals])
+        n = len(x_vals) - 1
+        y = np.zeros(n + 1)
+
+        y = method(y, h, len(x_vals) - 1, start, b)
+        err = calc_err(y_analytics, y, 'none', h, None)
+    
+    return h, err
+
 
 # class to avoid static and global values
 class Solver():
     def __init__(self, filename: str) -> None:
-        r, v0, s_max, h = read_input(filename)
+        debug, r, v0, s_max, h = read_input(filename)
+        self.debug = debug
         self.r = r
         self.v0 = v0
         self.h = h
@@ -60,9 +117,6 @@ class Solver():
         self.euler_vals = [np.zeros(self.n[i] + 1) for i in range(len(self.h))]
         self.adams_vals = [np.zeros(self.n[i] + 1) for i in range(len(self.h))]
 
-        self.euler_rich = [np.zeros(self.n[i] + 1) for i in range(1, len(self.h))]
-        self.adams_rich = [np.zeros(self.n[i] + 1) for i in range(1, len(self.h))]
-
         self.euler_errs = np.zeros(len(self.h))
         self.adams_errs = np.zeros(len(self.h))
 
@@ -73,33 +127,17 @@ class Solver():
             for s_val in self.s_vals
         ]
 
-    def solve_euler(self, ind: int):
-        v_vals = np.zeros(self.n[ind] + 1)
-        v_vals[0] = self.v0
+    def solve_euler(self):
+        for i in range(len(self.h)):
+            v_vals = np.zeros(self.n[i] + 1)
+            v_vals = euler_round(v_vals, self.h[i], self.n[i], self.v0, self.r)
+            self.euler_vals[i] = v_vals
 
-        for i in range(self.n[ind]):
-            v_vals[i + 1] = v_vals[i] + self.h[ind] * rhs(v_vals[i], self.s_vals[ind][i]) 
-
-        self.euler_vals[ind] = v_vals
-
-    def solve_adams(self, ind):
-        v_vals = np.zeros(self.n[ind] + 1)
-        v_vals[0] = self.v0
-
-        # euler method for the fist 4 points
-        for i in range(3):
-            v_vals[i + 1] = v_vals[i] + self.h[ind] * rhs(v_vals[i], self.s_vals[ind][i]) 
-
-        # start adams method
-        for i in range(3, self.n[ind]):
-            v_vals[i + 1] = v_vals[i] + self.h[ind] / 24 * (
-                55 * rhs(v_vals[i], self.s_vals[ind][i]) -
-                59 * rhs(v_vals[i - 1], self.s_vals[ind][i - 1]) + 
-                37 * rhs(v_vals[i - 2], self.s_vals[ind][i - 2]) -
-                9 * rhs(v_vals[i - 3], self.s_vals[ind][i - 3])
-            )
-
-        self.adams_vals[ind] = v_vals
+    def solve_adams(self):
+        for i in range(len(self.h)):
+            v_vals = np.zeros(self.n[i] + 1)
+            v_vals = adams_round(v_vals, self.h[i], self.n[i], self.v0, self.r)
+            self.adams_vals[i] = v_vals
     
     def calc_errors(self):
         for i in range(len(self.n)):
@@ -145,7 +183,7 @@ class Solver():
         axs.grid(True)
         pass
 
-    def plot_errs(self, axs):
+    def plot_convergence(self, axs):
         log_h = np.log(self.h)
         log_euler = np.log(self.euler_errs)
         log_adams = np.log(self.adams_errs)
@@ -169,20 +207,51 @@ class Solver():
         axs.set_ylabel('log(eps)')
         axs.legend()
         axs.grid(True)
-                
-    def run(self, axs):
-        self.solve_analytics()
 
-        for i in range(len(self.h)):
-            self.solve_euler(i)
-            self.solve_adams(i)
+    def research_const(self, h, axs):
+        b_vals = np.array([self.r / 2, self.r, self.r * 2])
+        s_vals = np.arange(0, self.s_max + h, h)
+        n = len(s_vals) - 1
+
+        for b in b_vals:
+            y = np.zeros(n + 1)
+            y = adams_round(y, h, n, self.v0, b)
+            axs.plot(s_vals, y, label=f'Решения для r = {b}')
+
+        axs.set_title('Решения для разных констант')
+        axs.set_xlabel('S, м')
+        axs.set_ylabel('v(s), м/с')
+        axs.legend()
+        axs.grid(True)
+                
+    def run(self, axs, figs):
+        self.solve_analytics()
+        self.solve_euler()
+        self.solve_adams()
 
         self.calc_errors()
         self.plot_solve(axs[0])
-        self.plot_errs(axs[1])
+        self.plot_convergence(axs[1])
+
+        h_euler, err_euler = find_opt_grid(self.h[0], self.euler_errs[0], self.v0, self.s_max, self.r, euler_round)
+        h_adams, err_adams = find_opt_grid(self.h[0], self.adams_errs[0], self.v0, self.s_max, self.r, adams_round)
+
+        self.logger.warning("Optimized step | euler: %.4f | adams: %.4f |", h_euler, h_adams)
+        self.logger.warning("Error for step | euler: %.4f | adams: %.4f |", err_euler, err_adams)
+
+        self.research_const(h_adams, axs[2])
 
         plt.tight_layout()
-        plt.show()
+
+        if self.debug:
+            plt.show()
+        else:
+            if not os.path.exists('out'):
+                os.mkdir('out')
+            figs[0].savefig('out/solve.png', dpi=100)
+            figs[1].savefig('out/convergence.png', dpi=100)
+            figs[2].savefig('out/b-research.png', dpi=100)
+
 
     def set_logger(self, logger): self.logger = logger
 
@@ -208,11 +277,15 @@ def main():
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
-    _, axs = plt.subplots(2, 1, figsize=(10, 10))
+    logger.warning("Sympy analytics solution: %s", sp_solve.solve())
+
+    fig1, axs1 = plt.subplots()
+    fig2, axs2 = plt.subplots()
+    fig3, axs3 = plt.subplots()
 
     solver = Solver('input.json')
     solver.set_logger(logger)
-    solver.run(axs)
+    solver.run([axs1, axs2, axs3], [fig1, fig2, fig3])
 
 
 if __name__ == "__main__":
